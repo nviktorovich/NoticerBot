0a0a3b1ab7dca7c16e783f8e75d4cc30ea33d894
nmaslenko <nvmaslenko@gmail.com>
2021-12-17 15:37:19 +0500

    вернул инициализацию бота в функции BotDrive

--- demon/tg_demon.go
+++ demon/tg_demon.go
@@ -2,11 +2,17 @@ package demon
 
 import "github.com/nviktorovich/NoticerBot/service"
 
-func BotDrive(bot *service.TelegramBot, text string, rID int64) error {
+func BotDrive(text string, rID int64) error {
 
-	notice := service.NewNotice(text, rID)
-	if err := bot.Send(*notice); err != nil {
+	bot, err := service.NewTgBot("??????")
+	if err != nil {
 		return err
 	}
+
+	notice := service.NewNotice(text, rID)
+	if err = bot.Send(*notice); err != nil {
+		return err
+	}
+
 	return nil
 }
